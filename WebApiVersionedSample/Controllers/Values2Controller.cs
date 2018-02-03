c59b9a58f12dfeaad1d483618418abdbd4222f83
Frank DeCaire <frank@decaire.net>
2018-02-03 16:28:50 -0500

    Initial Check-in

---
+++ WebApiVersionedSample/Controllers/Values2Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiVersionedSample.Controllers
+{
+	[ApiVersion("2.0")]
+	[ApiVersion("2.1")]
+	[Route("v{version:apiVersion}/Values")]
+    public class Values2Controller : Controller
+    {
+        [HttpGet, MapToApiVersion("2.0")]
+        public IEnumerable<string> GetV20()
+        {
+            return new string[] { "value1 - version 2", "value2 - version 2" };
+        }
+
+	    [HttpGet, MapToApiVersion("2.1")]
+	    public IEnumerable<string> GetV21()
+	    {
+		    return new string[] { "value1 - version 2.1", "value2 - version 2.1" };
+	    }
+
+		[HttpGet("{id}", Name = "Get"), MapToApiVersion("2.0")]
+        public string Get20(int id)
+        {
+            return $"id={id} version 2.0";
+        }
+
+	    [HttpGet("{id}", Name = "Get"), MapToApiVersion("2.1")]
+	    public string Get21(int id)
+	    {
+		    return $"id={id} version 2.1";
+	    }
+    }
+}
